66b95ea45076a47633aaaa9fe10c7663fcdd9c5e
luc34fenos <fenos.rasolondraibe@gmail.com>
2019-03-27 08:48:49 +0100

    version1.0.2

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -105,5 +105,5 @@ Rails.application.configure do
       :enable_starttls_auto => true
   }
 
-  config.action_mailer.default_url_options = { host: 'https://sayna.herokuapp.com/' }
+  config.action_mailer.default_url_options = { host: 'localhost:3000' }
 end
