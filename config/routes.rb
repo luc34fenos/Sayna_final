67e991213426257b02522a28a9501c7ee24f42ae
hariniaina <mahefa.ramaharavo@yahoo.com>
2019-03-27 13:15:32 +0100

    page errors

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'home#index'
+  get '/404' , to: 'errors#not_found'
+  get '/422' , to: 'errors#unacceptable'
+  get '/404' , to: 'errors#server_errors'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
