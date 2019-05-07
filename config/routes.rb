098958df04596c8bb504e896d6c13d37241f41b1
hariniaina <mahefa.ramaharavo@yahoo.com>
2019-05-07 10:16:58 +0300

    front.2

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,8 @@ Rails.application.routes.draw do
   root 'home#index'
    get '/contact', to: 'home#contact'
    post '/sendmail', to: 'home#send_mail'
+   get '/equipe', to:'home#equipe'
+   get '/thp', to:'home#thp'
+   get '/simplon', to:'home#simplon'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
